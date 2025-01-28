8cdb51ac34b35ee2256a7f5bba26ab39e090abaa
D-ON-ST <mds7942123@gmail.com>
2025-01-28 20:15:09 +0900

    Update: Change background video to image

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,20 @@
 export default function Home() {
-    return (
-      <div className="relative w-full h-screen bg-black">
-        {/* 움직이는 배경 */}
-        <video
-          className="absolute top-0 left-0 w-full h-full object-cover"
-          src="/background.mp4"
-          autoPlay
-          loop
-          muted
-        />
-  
-        {/* 콘텐츠 */}
-        <div className="relative z-10 flex flex-col items-center justify-center h-full text-white">
-          <h1 className="text-4xl font-bold">반응형 웹 디자인</h1>
-          <p className="mt-4 text-lg">이곳에 주요 콘텐츠를 배치하세요.</p>
-        </div>
-  
-        {/* 하단 메뉴바 */}
-        <div className="fixed bottom-0 left-0 w-full bg-gray-800 bg-opacity-80">
-          <nav className="flex justify-around py-2">
-            <a href="#" className="text-white">홈</a>
-            <a href="#" className="text-white">소개</a>
-            <a href="#" className="text-white">연락처</a>
-          </nav>
-        </div>
+  return (
+    <div className="relative w-full h-screen bg-black bg-cover bg-center" style={{ backgroundImage: "url('/background.jpg')" }}>
+      {/* 콘텐츠 */}
+      <div className="relative z-10 flex flex-col items-center justify-center h-full text-white">
+        <h1 className="text-4xl font-bold">반응형 웹 디자인</h1>
+        <p className="mt-4 text-lg">이곳에 주요 콘텐츠를 배치하세요.</p>
       </div>
-    );
-  }
-  
+
+      {/* 하단 메뉴바 */}
+      <div className="fixed bottom-0 left-0 w-full bg-gray-800 bg-opacity-80">
+        <nav className="flex justify-around py-2">
+          <a href="#" className="text-white">홈</a>
+          <a href="#" className="text-white">소개</a>
+          <a href="#" className="text-white">연락처</a>
+        </nav>
+      </div>
+    </div>
+  );
+}
